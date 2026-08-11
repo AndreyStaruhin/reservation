333aa11b4fec1a6b35a845c5a44bae7881a86a9b
Andrew <andrey.staruhin@gmail.com>
2026-08-11 23:54:12 +0700

    fix: fix package name

--- src/test/java/dev/andrey/ReservationSmokeTest.java
+++ src/test/java/dev/andrey/ReservationSmokeTest.java
@@ -1,15 +1,12 @@
-package andrey;
+package dev.andrey;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import dev.andrey.Reservation;
 
 public class ReservationSmokeTest {
     
